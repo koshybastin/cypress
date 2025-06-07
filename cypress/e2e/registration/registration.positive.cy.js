10407e40c6c42fe484dfca732781deccf6a63b86
Mary Koshy <koshybastin@gmail.com>
2025-06-07 15:26:39 +0200

    Add more test

--- cypress/e2e/registration/registration.positive.cy.js
+++ cypress/e2e/registration/registration.positive.cy.js
@@ -5,6 +5,7 @@
 import { faker } from '@faker-js/faker';
 
 const email = faker.internet.email('Mary', undefined, 'example.com');
+const password = faker.string.uuid();
 
 describe('Registration', () => {
   beforeEach(() => {
@@ -14,24 +15,38 @@ describe('Registration', () => {
 
   it('should register a new user account', () => {
     
+    //Register a new user
 
-    cy.get('[href="/auth/register"]').click()
-    cy.get('[name="firstName"]').type('Mary')
-    cy.get('[name="lastName"]').type('Koshy')
-    cy.get('[name="email"]').type(email)
-    cy.get('[name="password"]').type('Rea@1234')
-    cy.get('[type="submit"]').click()
+    cy.get('[href="/auth/register"]').click();
+    cy.get('[name="firstName"]').type('Mary');
+    cy.get('[name="lastName"]').type('Koshy');
+    cy.get('[name="email"]').type(email);
+    cy.get('[name="password"]').type(password);
+    cy.get('[type="submit"]').click();
 
+
+    //verifying the user role,name,url and page title
     
-    cy.get('a p').should('have.text', 'role: user')
+    cy.get('a p').should('have.text', 'role: user');
+    cy.get('h6').should('have.text','Mary  Koshy');
+    cy.title().should('eq','User: Profile | Delek Homes');
+    cy.url().should('include','dashboard/user/profile');
 
-    cy.get('h6').invoke('text').then((text) => {
-  
-      cy.log('campaign', - `${text}-`)
+    //click user icon and logout
+    cy.get('button [data-testid="PersonIcon"]').click();
+    cy.contains('Logout').click();
+
+
+    //login as user
+
+     cy.get('[name="email"]').type(email)
+     cy.get('[name="password"]').type(password)
+    //login user
+     cy.contains('Login').click()
+     //verify the role user
+     cy.get('a p').should('have.text', 'role: user');
+
     })
-    //cy.title().should('eq','User: Profile | Delek Homes')
+   
   })
   
-
-  /* ==== Test Created with Cypress Studio ==== */
-})
